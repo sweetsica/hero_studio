21eb9e318f121399a34b32d6f9df2902073f6ff4
sweetsica <barondeepsnow@gmail.com>
2022-10-28 10:50:44 +0700

    fix title edit member

--- resources/views/admin-template/page/member/edit.blade.php
+++ resources/views/admin-template/page/member/edit.blade.php
@@ -10,7 +10,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box">
-                        <h4 class="page-title">File Manager</h4>
+                        <h4 class="page-title">Thông tin nhân sự</h4>
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
                                 <li class="breadcrumb-item"><a href="javascript:void(0);">Shreyu</a></li>
