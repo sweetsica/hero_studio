92ac6eabd43c0335d4367b6c0ad160198db14eec
sweetsica <barondeepsnow@gmail.com>
2022-11-03 02:20:25 +0700

    check

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -24,8 +24,6 @@ class NotifyController extends Controller
         $returnData = [
             'notifies' => $nofifies
         ];
-
-
         return view('admin-template.page.notify.create', $returnData);
     }
 
