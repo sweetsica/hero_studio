e4bf4c70407d230c9ff36bcd4d9dd7997dc3cd9b
skyfury2651 <skyfury2651@gmail.com>
2022-11-05 15:43:20 +0700

    feature: test

--- app/Exports/ReportExport.php
+++ app/Exports/ReportExport.php
@@ -4,13 +4,26 @@ namespace App\Exports;
 
 use App\Models\Task;
 use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
-class ReportExport implements FromArray
+class ReportExport implements FromArray, WithMultipleSheets
 {
     public function array(): array
     {
        $data = Task::query()->get()->toArray();
 
         return $data;
+    }
+
+    public function sheets(): array
+    {
+        $sheets = [];
+
+        for ($month = 1; $month <= 12; $month++) {
+            $sheets[] = new InvoicesPerMonthSheet($this->year, $month);
+        }
+
+        return $sheets;
+        // TODO: Implement sheets() method.
     }
 }
